662e81a1f75d43a7331e43bc04b550fea65a8904
fatemeh <fatemeh.abolhasanzade@gmail.com>
2021-12-29 15:42:17 +0330

    input and fetche taskes added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
-import React, { useState } from "react";
-import { TextField, Button } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { TextField, Button, listClasses } from '@mui/material';
 import { firebase } from "firebase";
 import { db } from "./firebase-config";
 
 function App() {
   const [task, setTask] = useState("");
+  const [taslList, setTaslList] = useState("");
+  useEffect(() => {
+    getTasks();
+  }, [task])
 
   const addTask = (e) => {
     e.preventDefault();
     console.log("you are trying to add a task");
-    db.collection("Todo Task").add({
+    db.collection("TodoTasks").add({
       inProgress: true,
       Timestamp: firebase.firestore.FieldValue.sreverTimeStamp(),
       Task: task
     })
     setTask("");
+  }
+  const getTasks = () => {
+    db.collection("TodoTasks").onSnapshot((x) => {
+      setTaslList(x.docs.map((docs) => ({
+        id: docs.id,
+        Task: docs.data().Task,
+        inProgress: docs.data().inProgress
+      })));
+    })
   }
 
   return (
@@ -25,12 +38,16 @@ function App() {
         <form>
           <TextField id="outlined-basic" className="task-input" label="write a task" variant="outlined" />
           <Button type="submit" onClick={addTask} style={{ display: "none" }} variant="outlined">Outlined</Button>
-
+          {/* show tasks */}
+          {taslList.map((taskItem) => (
+            <p>{taskItem.Task}</p>
+          ))}
         </form>
       </div>
 
     </div>
   );
 }
+
 
 export default App;
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,8 @@
-
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 const firebaseConfig = {
     apiKey: "AIzaSyBUD50YTeeTIC2tzGSt_qXVxzknPr7xD5g",
     authDomain: "todo-app-2ac27.firebaseapp.com",
@@ -10,8 +12,16 @@ const firebaseConfig = {
     appId: "1:431291926361:web:d1656fbb2888e7c7f8ae48",
     measurementId: "G-9TTBVL0E1D"
 };
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export { db };
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
+
+// export { db };
