7bb3ed2c298fa8a2f6d448422d3d07d61b0d5c71
fatemeh <fatemeh.abolhasanzade@gmail.com>
2021-12-29 11:12:10 +0330

    todo app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import { TextField, Button } from '@mui/material';
+import { firebase } from "firebase";
+import { db } from "./firebase-config";
 
 function App() {
+  const [task, setTask] = useState("");
+
+  const addTask = (e) => {
+    e.preventDefault();
+    console.log("you are trying to add a task");
+    db.collection("Todo Task").add({
+      inProgress: true,
+      Timestamp: firebase.firestore.FieldValue.sreverTimeStamp(),
+      Task: task
+    })
+    setTask("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <header><h1>todolist application</h1></header>
+        <form>
+          <TextField id="outlined-basic" className="task-input" label="write a task" variant="outlined" />
+          <Button type="submit" onClick={addTask} style={{ display: "none" }} variant="outlined">Outlined</Button>
+
+        </form>
+      </div>
+
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+const firebaseConfig = {
+    apiKey: "AIzaSyBUD50YTeeTIC2tzGSt_qXVxzknPr7xD5g",
+    authDomain: "todo-app-2ac27.firebaseapp.com",
+    projectId: "todo-app-2ac27",
+    storageBucket: "todo-app-2ac27.appspot.com",
+    messagingSenderId: "431291926361",
+    appId: "1:431291926361:web:d1656fbb2888e7c7f8ae48",
+    measurementId: "G-9TTBVL0E1D"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+export { db };
