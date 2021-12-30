5457b22fcc9075f3cc75e972fc2ed63d1749956b
fatemeh <fatemeh.abolhasanzade@gmail.com>
2021-12-30 11:17:38 +0330

    "db.collection is not a function" error!!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
+import "./App.css";
 import React, { useState, useEffect } from "react";
-import { TextField, Button, listClasses } from '@mui/material';
-import { firebase } from "firebase";
+import { TextField, Button } from "@mui/material";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import { db } from "./firebase-config";
 
 function App() {
   const [task, setTask] = useState("");
-  const [taslList, setTaslList] = useState("");
-  useEffect(() => {
-    getTasks();
-  }, [task])
+  const [taskList, setTaskList] = useState("");
+  // useEffect(() => {
+  //   getTasks();
+  // }, [task])
 
   const addTask = (e) => {
     e.preventDefault();
@@ -21,15 +23,17 @@ function App() {
     })
     setTask("");
   }
-  const getTasks = () => {
-    db.collection("TodoTasks").onSnapshot((x) => {
-      setTaslList(x.docs.map((docs) => ({
-        id: docs.id,
-        Task: docs.data().Task,
-        inProgress: docs.data().inProgress
-      })));
-    })
-  }
+  // const getTasks = () => {
+  //   db.collection("TodoTasks").onSnapshot((querySnapshot) => {
+  //     let result = querySnapshot.docs.map((doc) => ({
+  //       id: doc.id,
+  //       Task: doc.data().Task,
+  //       inProgress: doc.data().inProgress
+  //     }))
+  //     setTaskList(result);
+  //   })
+  // }
+  // console.log(taskList);
 
   return (
     <div className="App">
@@ -39,9 +43,13 @@ function App() {
           <TextField id="outlined-basic" className="task-input" label="write a task" variant="outlined" />
           <Button type="submit" onClick={addTask} style={{ display: "none" }} variant="outlined">Outlined</Button>
           {/* show tasks */}
-          {taslList.map((taskItem) => (
-            <p>{taskItem.Task}</p>
-          ))}
+          {/* {taskList.map((taskItem) => {
+            return (
+              <p>{taskItem.Task}</p>
+            )
+          }
+
+          )} */}
         </form>
       </div>
 
